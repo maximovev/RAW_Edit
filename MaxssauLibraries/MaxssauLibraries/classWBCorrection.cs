66569f3b03aedd46b3c1c59b1857f3a754d55089
Evgeny M. <evgeniy.maximov@pegas-agro.ru>
2025-03-25 15:31:25 +0400

    добавлен класс цветовой температуры

---
+++ MaxssauLibraries/MaxssauLibraries/classWBCorrection.cs
@@ -0,0 +1,45 @@
+﻿using image_designer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxssauLibraries
+{
+    public class classWBCorrection
+    {
+        private classLogger Logger;
+
+        public classWBCorrection(classLogger logger)
+        {
+            Logger = logger;
+        }
+
+        public OperationStatus Calculate(ref RGB_Pixel rgb_pixel, int color_temperarue)
+        {
+            try
+            {
+
+
+
+                return OperationStatus.STATUS_OK;
+            }
+            catch (Exception ex)
+            {
+                if (Logger != null)
+                {
+                    if (Logger.status == classLogger.STATUS.OPEN)
+                    {
+                        Logger.add_to_log("WB Calculator", ex.Message);
+                        if (ex.StackTrace != null)
+                        {
+                            Logger.add_to_log("WB Calculator", ex.StackTrace);
+                        }
+                    }
+                }
+                return OperationStatus.STATUS_FAIL;
+            }
+        }
+    }
+}
